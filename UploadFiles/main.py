70105807d4be7b47be2c0b77407266d3726f3d44
Roshini-2100 <1926ja38@kgcas.com>
2021-10-16 13:01:37 +0530

    Upload Photos and Files in SocialMedia Using Python.

---
+++ UploadFiles/main.py
@@ -0,0 +1,6 @@
+from instabot import Bot
+bot = Bot()
+bot.login(username="kanagarajroshini2100", password="Qwerty@123")
+
+bot.upload_photo("Gallery.jpg", caption="World")
+bot.upload_video("BORINGLIFE.mp4", caption="FunWorld")
